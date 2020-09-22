62fee833b92d06b2016e98067bb58134307fe713
Victor-LEONARD <leonard@abvent.fr>
2020-09-22 16:49:07 +0200

    feat(component): connect

---
+++ src/pages/Connect.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page padding class="docs-input row justify-center">
+        <div class="q-gutter-md" style="width: 500px; max-width: 90vw;">
+          <q-input v-model="form.email" type="email" label="Email" />
+          <q-input v-model="form.password" type="password" label="Password" />
+          <q-btn @click="connect" label="Connect" />
+          <q-btn to="register" label="Register" flat />
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+// import UserService from '../services/UserService'
+export default {
+  name: 'connect',
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+      tempUser: undefined,
+      modal: false,
+      users: [],
+      currentUser: localStorage.getItem('userId'),
+      currentUserName: localStorage.getItem('userName')
+    }
+  },
+  methods: {
+    connect () {
+      /* UserService.logIn(this.form.email, this.form.password)
+        .then(resp => {
+          console.log('resp, resp')
+        })
+        .catch(error => {
+          let er = JSON.stringify(error)
+          let er2 = JSON.parse(er)
+          console.log(error['message'], er2.response.data.error.message)
+        }) */
+      this.$store
+        .dispatch('main/connectUser', {
+          email: this.form.email,
+          password: this.form.password
+        })
+        .then(resp => {
+          setTimeout(() => {
+            this.$router.push({
+              name: 'home'
+            })
+          }, 500)
+        })
+        .catch(error => {
+          this.$store.dispatch('main/changeLoadingState', false)
+          const er = JSON.stringify(error)
+          const er2 = JSON.parse(er)
+          const messageError = er2.response.data.error.message
+          console.log(error.message, er2.response.data.error.message)
+          this.$q.dialog({
+            title: 'Error',
+            message: messageError
+          })
+        })
+      /* Api().post('/user', {
+        'logemail': this.form.logemail,
+        'logpassword': this.form.logpassword
+      })
+        .then(resp => {
+          console.log('resp', resp)
+        }) */
+      /* Api().get('/profile')
+        .then(resp => {
+          console.log('get profile', resp)
+        }) */
+    },
+    identify (user) {
+      this.tempUser = user
+      this.modal = true
+    }
+  },
+  mounted () {
+    /* Api().get('users')
+      .then(resp => {
+        this.users = resp.data.users
+      }) */
+  }
+}
+</script>
