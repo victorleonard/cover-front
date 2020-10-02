6ba8f33f2237b87f1fa7b3826e07bc1d47df05c8
Victor-LEONARD <leonard@abvent.fr>
2020-10-02 21:23:43 +0200

    get user info on start

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="q-app">
+    <!-- <q-dialog class="q-pa-md q-gutter-sm" v-model="dialog" minimized position="top">
+      <q-card>
+        <q-card-section class="row items-center no-wrap">
+          <div>
+            <div class="text-weight-bold">Titre</div>
+            <div class="text-grey">Erreur</div>
+          </div>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog> -->
     <router-view />
   </div>
 </template>
@@ -10,15 +23,29 @@ import { Loading } from 'quasar'
 
 export default {
   name: 'App',
-  computed: mapState('main', ['loading', 'user']),
+  data: () => ({
+    dialog: true
+  }),
+  computed: mapState('main', ['loading', 'user', 'error']),
   watch: {
+    error: (val) => {
+      if (val.type === true) {
+        this.dialog = true
+      }
+    },
     loading: function (val) {
+      console.log('loading', val)
       if (val) {
         Loading.show()
       } else {
         Loading.hide()
       }
     }
+  },
+  beforeCreate () {
+    this.$store.dispatch('main/getMe')
+    this.$store.dispatch('main/getMyProfile')
+    this.$store.dispatch('main/changeLoadingState', false)
   }
 }
 </script>
