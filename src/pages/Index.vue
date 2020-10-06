8f72d50ce1a3e4be727b805b4bc92eeecf5e9092
Victor-LEONARD <leonard@abvent.fr>
2020-10-06 16:33:08 +0200

    dev

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="fit column wrap justify-center items-center content-center">
+    <div class="row"></div>
+    <h5>Bienvenue</h5>
+    <div>
+      <q-btn :to="{ name: 'connect' }" color="primary" text-color="white" label="Connexion" />
+      <q-btn class="q-ml-sm" :to="{ name: 'register' }" color="white" text-color="black" label="Inscripition" />
+    </div>
   </q-page>
 </template>
 
