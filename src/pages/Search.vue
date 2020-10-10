17eecfaa398d864ee81f57afa04e34c5ada1df93
Victor-LEONARD <leonard@abvent.fr>
2020-10-10 22:59:59 +0200

    style(search)

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -2,7 +2,7 @@
   <q-page padding class="search-page q-pl-md q-pr-md">
     <!-- DIALOG selection -->
     <q-dialog v-model="songSelectDialog">
-      <q-card class="my-card" v-if="songSelected">
+      <q-card class="my-card" v-if="songSelected" style="width: 70vw">
         <q-img :src="songSelected.album.images[0].url" />
 
         <q-card-section>
@@ -34,27 +34,32 @@
     <div class="q-mt-md">
       <q-spinner-puff class="spinner" v-if="search.length && loading" color="secondary" :size="50" />
       <q-list v-if="searchResult.length">
-        <q-expansion-item class="shadow-1 q-mb-md" v-for="res in searchResult" :key="res.id">
-        <template v-slot:header>
+        <q-expansion-item v-for="res in searchResult" :key="res.id" class="shadow-1 q-mb-md">
+        <template v-slot:header style="padding: 0">
           <q-item-section avatar>
-            <q-avatar square>
-            <img :src="res.album.images[1].url" alt=""/>
+            <q-avatar square size="80px">
+              <img :src="res.album.images[1].url" alt="">
             </q-avatar>
           </q-item-section>
 
           <q-item-section>
-            {{ res.name }}<br>
-            {{ res.artists[0].name }}
+            <q-item-label overline>{{res.name}}</q-item-label>
+            <q-item-label>{{res.artists[0].name}}</q-item-label>
           </q-item-section>
         </template>
-          <q-card-actions align="around">
+
+        <q-card>
+          <q-card-section>
+            <q-card-actions align="around">
             <q-btn flat icon="fab fa-spotify" @click="launchSpotify(res.uri)" size="md" color="positive"></q-btn>
             <q-btn class="btn-audio btn-audio-play" :id="'btn-audio-play-'+res.id" v-if="res.preview_url" flat color="primary" label="" icon="ion-md-play" size="md" @click="play(res.id)" />
             <q-btn class="btn-audio btn-audio-pause" :id="'btn-audio-pause-'+res.id" v-if="res.preview_url" flat color="primary" label="" icon="ion-md-pause" size="md" @click="pause(res.id)" />
             <q-btn flat color="primary" label="Choisir" icon="ion-md-add-circle" size="md" @click="displaySelectDialog(res)" />
             <audio :src="res.preview_url" :id="'audio-' + res.id" type="audio/mpeg"></audio>
           </q-card-actions>
-        </q-expansion-item>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
       </q-list>
     </div>
   </q-page>
