ae2a0af5be792ad7a381bde857ca0c5959c66b53
Victor-LEONARD <leonard@abvent.fr>
2020-10-20 21:40:46 +0200

    style(register)

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,28 +1,53 @@
 <template>
-  <q-page padding class="docs-input row justify-center">
-    <div class="q-gutter-md" style="width: 500px; max-width: 90vw;">
-      <q-input v-model="form.userName" label="Pseudo" />
-      <q-input v-model="form.email" type="email" label="Email" />
-      <q-input v-model="form.password" type="password" label="Password" />
-      <q-input
-        v-model="form.passwordConf"
-        type="password"
-        label="Password confirmation"
-      />
-      <q-btn @click="register" label="Create" />
-    </div>
+  <q-page class="welcome fit column wrap justify-center items-center content-center" style="background-color: #FAFAFA;">
+    <q-btn v-go-back=" '/' " align="right" class="go-back absolute-top-left" icon="fas fa-chevron-left" no-caps flat label="Back" />
+    <q-btn @click="sendEmailConfirmation" color="brand" no-caps unelevated label="Valider" />
+      <q-form
+      @submit="register"
+      class="q-gutter-md"
+      style="width: 500px; max-width: 90vw;"
+      >
+        <q-input
+          v-model="form.userName"
+          label="Pseudo"
+          :rules="[ val => val && val.length > 0 || 'Veuillez saisir un pseudo']"
+        />
+
+        <q-input
+          v-model="form.email"
+          type="email"
+          label="Email"
+          :rules="[ val => val && val.length > 0 || 'Veuillez saisir un email']"/>
+
+        <q-input
+          v-model="form.password"
+          type="password"
+          label="Mot de passe"
+          :rules="[ val => val && val.length > 0 || 'Veuillez saisir un mot de passe']"
+        />
+
+        <q-input
+          v-model="form.passwordConf"
+          type="password"
+          label="Confirmer le mot de passe"
+          :rules="[ val => val && val.length > 0 || 'Veuillez confirmer votre mot de passe']"
+        />
+        <q-btn type="submit" color="brand" no-caps unelevated label="Valider" />
+      </q-form>
   </q-page>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
       form: {
-        userName: 'vic',
+        userName: 'Victor',
         email: 'vicleonard@yahoo.fr',
-        password: 'test',
-        passwordConf: 'test'
+        password: 'azerty',
+        passwordConf: 'azerty'
       },
       tempUser: undefined,
       modal: false,
@@ -32,8 +57,14 @@ export default {
     }
   },
   methods: {
+    sendEmailConfirmation () {
+      axios.post('https://cover-mobile.herokuapp.com/auth/send-email-confirmation', {
+        email: this.form.email
+      })
+    },
     register () {
       if (this.form.password === this.form.passwordConf) {
+        this.$store.dispatch('main/changeLoadingState', true)
         this.$store
           .dispatch('main/register', {
             username: this.form.userName,
@@ -41,10 +72,11 @@ export default {
             password: this.form.password
           })
           .then(resp => {
+            this.$store.dispatch('main/changeLoadingState', false)
             this.$q
               .dialog({
-                title: 'Success',
-                message: 'Welcome ' + this.form.userName + ' !'
+                title: 'Votre compte a été créé',
+                message: 'Un email de confirmation vous a été envoyé. Veuillez cliquer sur le lien présent dans cet email pour valider votre compte'
               })
               .onOk(() => {
                 this.$router.push({
@@ -59,7 +91,12 @@ export default {
               })
           })
           .catch(error => {
-            console.log('error !!!', error)
+            this.$q.notify({
+              type: 'negative',
+              message: error.response.data.data[0].messages[0].message,
+              position: 'top'
+            })
+            this.$store.dispatch('main/changeLoadingState', false)
             /* this.$store.dispatch('main/changeLoadingState', false)
             let er = JSON.stringify(error)
             let er2 = JSON.parse(er)
@@ -71,7 +108,11 @@ export default {
             }) */
           })
       } else {
-        alert('error password confirmation')
+        this.$q.notify({
+          type: 'negative',
+          message: 'Vous n\'avez pas correctement confirmer votre mot de passe',
+          position: 'top'
+        })
       }
     },
     identify (user) {
