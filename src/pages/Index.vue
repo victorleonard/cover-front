c0d4380da2caba484e7f491ffa7722791550f4d1
Victor-LEONARD <leonard@abvent.fr>
2020-10-29 22:10:39 +0100

    fix image url

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <q-page class="welcome fit column wrap justify-center items-center content-center">
     <div class="row">
-      <img class="logo" src="/cover.png" alt="">
+      <img class="logo" src="https://cover.s3.eu-west-3.amazonaws.com/cover_28108a73e4.png" alt="">
     </div>
     <div class="q-mt-xl">
-      <q-btn :to="{ name: 'connect' }" icon="fas fa-door-open" outline no-caps unelevated color="brand" text-color="white" label="Enter" />
+      <q-btn :to="{ name: 'home' }" icon="fas fa-door-open" outline no-caps unelevated color="brand" text-color="white" label="Entrer" />
       <!-- <q-btn class="q-ml-sm" :to="{ name: 'register' }" unelevated color="white" text-color="white" flat no-caps label="Inscription" /> -->
     </div>
   </q-page>
@@ -18,7 +18,7 @@ export default {
 
 <style lang="scss" scoped>
   .welcome {
-    background-image: url('/nick-moore-5zWN8DTNeVA-unsplash.jpg');
+    background-image: url('https://cover.s3.eu-west-3.amazonaws.com/bg_home_d164df054e.jpg');
     background-size: cover;
   }
   .logo {
