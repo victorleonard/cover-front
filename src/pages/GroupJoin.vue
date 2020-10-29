d812edbd066c6795d1e62718134226151d7bf219
Victor-LEONARD <leonard@abvent.fr>
2020-10-29 22:08:46 +0100

    fix

--- src/pages/GroupJoin.vue
+++ src/pages/GroupJoin.vue
@@ -2,8 +2,6 @@
 <q-page padding>
   <q-dialog v-model="askInvitationDialog">
       <q-card v-if="groupSelected" class="my-card" style="width: 70vw">
-        <!-- <q-img :src="songSelected.album.images[0].url" /> -->
-
         <q-card-section>
           <div class="row no-wrap items-center">
             <div class="text-h6">
@@ -28,11 +26,12 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
+
   <div class="row">
     <div class="col">
     <h4 class="q-mt-md q-mb-md">Liste des groupes</h4>
-    <q-card class="my-card" v-for="group in groups" :key="group.id">
-      <img :src="group.image.url" alt="group image">
+    <q-card class="my-card q-mb-lg" v-for="group in groups" :key="group.id">
+      <img :src="getBestFormatImage(group.image.formats)" alt="group image">
 
       <q-card-section>
         <div class="text-h6">{{ group.name }}</div>
@@ -40,16 +39,19 @@
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        <q-chip :icon="!user.avatar ? 'account_circle' : null" v-for="user in group.users" :key="user.id">
-          <q-avatar v-if="user.avatar">
-            <img :src="user.avatar.url" alt="avatar">
+        <q-chip v-for="user in group.users" :key="user.id">
+          <q-avatar v-if="user.profile.avatar">
+            <img :src="user.profile.avatar.url" alt="avatar">
           </q-avatar>
-          {{ user.username }}
+          {{ user.profile.pseudo }}
         </q-chip>
       </q-card-section>
-      <q-card-actions align="right">
-        <q-btn v-if="!alreadyAsk(group.id)" @click="askInvitationDialog = true, groupSelected = group" round color="primary" icon="east" />
-        <div v-else>Demande en Attente...</div>
+      <q-card-actions align="right" v-if="isAlreadyMember(group.id)">
+        <div><q-chip color="green" text-color="white" icon="fas fa-guitar" label="Déjà membre" /></div>
+      </q-card-actions>
+      <q-card-actions align="right" v-else>
+        <q-btn v-if="!alreadyAsk(group.id)" @click="askInvitationDialog = true, groupSelected = group" color="primary" label="rejoindre" icon-right="fas fa-caret-right" />
+        <div v-else><q-chip color="red" text-color="white" icon="fas fa-clock" label="Demande en attente" /></div>
       </q-card-actions>
     </q-card>
     </div>
@@ -72,6 +74,18 @@ export default {
     ...mapState('main', ['groups', 'user', 'myAskingInvitations'])
   },
   methods: {
+    getBestFormatImage (formats) {
+      if (formats.medium) {
+        return formats.medium.url
+      } else if (formats.small) {
+        return formats.small.url
+      } else {
+        return formats.thumbnail.url
+      }
+    },
+    isAlreadyMember (groupId) {
+      return this.user.groups.find(g => g === groupId)
+    },
     alreadyAsk (groupId) {
       return this.myAskingInvitations.find(a => a.group.id === groupId)
     },
