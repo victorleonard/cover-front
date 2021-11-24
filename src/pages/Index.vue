ce0e72406814e5f00bbcd1aac2eb8d2739b3086c
Victor LEONARD <victorleonard@hotmail.fr>
2021-11-24 11:07:25 +0100

    style() index icon

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <q-page class="welcome fit column wrap justify-center items-center content-center">
     <div class="row">
-      <img class="logo" src="https://cover.s3.eu-west-3.amazonaws.com/cover_28108a73e4.png" alt="">
+      <img class="logo" src="~assets/cover_logo_full_2.png" style="max-width: 150px" alt="">
     </div>
     <div class="q-mt-xl">
-      <q-btn :to="{ name: 'home' }" icon="fas fa-door-open" outline no-caps unelevated color="brand" text-color="white" label="Entrer" />
+      <q-btn :to="{ name: 'home' }" icon="eva-arrow-right-outline" size="lg" round outline no-caps  color="brand" text-color="white" />
       <!-- <q-btn class="q-ml-sm" :to="{ name: 'register' }" unelevated color="white" text-color="white" flat no-caps label="Inscription" /> -->
     </div>
   </q-page>
@@ -18,10 +18,8 @@ export default {
 
 <style lang="scss" scoped>
   .welcome {
-    background-image: url('https://cover.s3.eu-west-3.amazonaws.com/bg_home_d164df054e.jpg');
-    background-size: cover;
-  }
-  .logo {
-    filter: invert(1);
+   /*  background-image: url('https://cover.s3.eu-west-3.amazonaws.com/bg_home_d164df054e.jpg');
+    background-size: cover; */
+    background-color: rgba(0,0,0,0.9);
   }
 </style>
